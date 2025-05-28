f7d49a0a7c37ee71a6d3a3fe67a87a39a5752854
管晨曦 <guanchengxi1@outlook.com>
2025-05-28 11:59:14 +0800

    Update forced_pendulum_student.py

--- project_3_forced_pendulum/forced_pendulum_student.py
+++ project_3_forced_pendulum/forced_pendulum_student.py
@@ -25,10 +25,12 @@ def solve_pendulum(l=0.1, g=9.81, C=2, Omega=5, t_span=(0,100), y0=[0,0]):
         return forced_pendulum_ode(t,state,l,g,C,Omega)
 
     #调用solve_ivp求解
-    sol = solve_ivp(ode_func,t_span,y0,
-                    t_eval=np.linspace(t_span[0],t_span[1],2000),  #生成1000个时间点
+    sol = solve_ivp(y:ode_func,
+                    t_span,y0,
+                    t_eval=np.linspace(t_span[0],t_span[1],2000),  #生成2000个时间点
                     method='Radau',  #适用于刚性问题
-                    rtol=1e-6  #控制精度
+                    rtol=1e-6
+                    atol=1e-9#控制精度
                  )
     return sol.t,sol.y[0]  #返回时间和角度
 
@@ -41,13 +43,14 @@ def find_resonance(l=0.1, g=9.81, C=2, Omega_range=None, t_span=(0,200), y0=[0,0
     # TODO: 实现共振频率查找功能
     # 提示: 需要调用solve_pendulum函数并分析结果
     if Omega_range is None:
-        Omega_range=np.linspa(0.5,8,50)  #默认扫描范围
+        Omage0=np.sqrt(g/l)  #小角度近似下的自然频率
+        Omega_range=np.linspa(Omega0/2,2*Omega0,50)  #默认扫描范围
 
     amplitudes=[]
     for Omega in Omega_range:
         #求解单摆运动
         t,theta =solve_pendulum(1,g,C,Omega,t_span,y0)
-        #计算稳态振幅（取后1/3数据）
+        #计算稳态振幅（取后1/2数据）
         steady_start=int(0.66*len(theta))
         steady_theta=theta[steady_start:]
         #计算峰峰值并取半作为振幅
@@ -71,7 +74,7 @@ def main():
     """主函数"""
     # 任务1: 特定参数下的数值解与可视化
     # TODO: 调用solve_pendulum和plot_results
-    t,theta = solve_pendulum(Omega=5)
+    t,theta = solve_pendulum()
     plot_results(t,theta,"Forced Pendulum (Omega=5)")
 
     # 任务2: 探究共振现象
@@ -96,6 +99,7 @@ def main():
     #找到共振频率并绘制其时间序列
     resonance_Omega=Omega_range[np.argmax(amplitudes)]
     t_res,theta_res=solve_pendulum(Omega=resonance_Omega)
+    print(f'Resonance frequency:({t_res:.2f}rad/s')
     plot_results(t_res,theta_res,f"Resonance at Omega={resonance_Omega:.2f}rad/s")
 
 
